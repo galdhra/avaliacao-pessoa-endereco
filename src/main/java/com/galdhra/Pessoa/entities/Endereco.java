81f92c9298a9885d70b83979614547bbd8cbfa76
Luiz Fernando Azevedo <luiz1337@gmail.com>
2023-02-10 19:13:44 -0300

    entities

---
+++ src/main/java/com/galdhra/Pessoa/entities/Endereco.java
@@ -0,0 +1,93 @@
+package com.galdhra.Pessoa.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tb_endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String logradouro;
+    private String cep;
+    private Integer numero;
+    private Boolean principal;
+    private String cidade;
+
+    private UnidadeFederativa UF;
+
+    @ManyToOne
+    @JoinColumn(name = "enderecos_id")
+    private Pessoa morador;
+
+    public Endereco (){
+
+    }
+
+    public Endereco(Long id, String logradouro, String cep, Integer numero, Boolean principal, String cidade, UnidadeFederativa UF) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.cep = cep;
+        this.numero = numero;
+        this.principal = principal;
+        this.cidade = cidade;
+        this.UF = UF;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Boolean getPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(Boolean principal) {
+        this.principal = principal;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Pessoa getMorador() {
+        return morador;
+    }
+
+    public void setMorador(Pessoa morador) {
+        this.morador = morador;
+    }
+}
---
+++ src/main/java/com/galdhra/Pessoa/entities/Pessoa.java
@@ -0,0 +1,59 @@
+package com.galdhra.Pessoa.entities;
+
+import jakarta.persistence.*;
+
+import java.time.*;
+import java.util.*;
+
+@Entity
+@Table(name = "tb_pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private LocalDate dataDeNascimento;
+
+
+    @OneToMany(mappedBy = "morador",cascade=CascadeType.PERSIST)
+    private List<Endereco> enderecos = new ArrayList<>();
+
+    public Pessoa(){
+
+    }
+
+    public Pessoa(Long id, String nome, LocalDate dataDeNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+}
